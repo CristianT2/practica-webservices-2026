aa4016705b88d0abeb47ce8c70ada9344ced3666
CristianT2 <c.torrejon96@gmail.com>
2026-05-14 22:30:55 -0300

    Se diseñaron el header y footer de la app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterModule, Header, Footer],
+  styleUrl: './app.css',
+  template: `
+    <header>
+      <app-header/>
+    </header>
+    <main>
+      <router-outlet/>
+    </main>
+    <footer>
+      <app-footer/>
+    </footer>
+  `   
+
 })
 export class App {
   protected readonly title = signal('tp-webservice-3305');
